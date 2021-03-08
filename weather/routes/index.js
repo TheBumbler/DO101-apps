b1733d6ad0b7f08f2f1205fa0cc5fe2717e44e68
The Bumbler <pete@henleyonline.co.uk>
2021-03-08 14:34:23 +0000

    and again...

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -7,13 +7,12 @@ const UNITS = process.env.UNITS || 'metric';
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { weather: null, err: null });
+  res.render('index', { weather: null, err: 'http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=${OWM_API_KEY}' });
 });
 
 router.post('/get_weather', async function (req,res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=${OWM_API_KEY}`;
-  res.render('index', {weather: null, error: url});
 
   try {
     let data = await fetch(url);
